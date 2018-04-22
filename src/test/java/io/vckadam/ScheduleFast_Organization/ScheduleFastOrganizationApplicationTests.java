fb0ffa5ffc827b7f4353f5383e95657c66b20ee5
vckadam <vckadam@asu.edu>
2018-04-21 20:44:18 -0400

    Initial commit

---
+++ src/test/java/io/vckadam/ScheduleFast_Organization/ScheduleFastOrganizationApplicationTests.java
@@ -0,0 +1,16 @@
+package io.vckadam.ScheduleFast_Organization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScheduleFastOrganizationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
